7010e2a653fcd0ae36f2ad63c8d8e0e877921cd2
Gabriel Galli <ggabriel96@hotmail.com>
2021-05-10 22:50:08 -0300

    Testing exclude parameter

--- tests/main/test_sqlalchemy_to_pydantic.py
+++ tests/main/test_sqlalchemy_to_pydantic.py
@@ -329,3 +329,29 @@ def test_keyed_column_schema() -> None:
         },
         "required": ["id"],
     }
+
+
+def test_exclude_removes_fields_from_generated_model() -> None:
+    # Arrange
+    Base = declarative_base()
+
+    class Test(Base):
+        __tablename__ = "test"
+
+        id = Column(Integer, primary_key=True)
+        number = Column(Integer)
+
+    # Act
+    TestPydantic = sqlalchemy_to_pydantic(Test, exclude={"number"})
+
+    # Assert
+    test = TestPydantic(id=1)
+    assert test.id == 1
+    assert TestPydantic.schema() == {
+        "title": "Test",
+        "type": "object",
+        "properties": {
+            "id": {"title": "Id", "type": "integer"},
+        },
+        "required": ["id"],
+    }
