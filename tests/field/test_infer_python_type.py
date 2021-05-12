43e990f68c0a1409192ac444956e2b7dc1440866
Gabriel Galli <ggabriel96@hotmail.com>
2021-05-11 21:35:00 -0300

    test_common_types

--- tests/field/test_infer_python_type.py
+++ tests/field/test_infer_python_type.py
@@ -1,24 +1,14 @@
 import datetime as dt
 import enum
+from decimal import Decimal
 from typing import List
 
 import pytest
-from sqlalchemy import ARRAY, Column, Enum, Integer, Interval
+from sqlalchemy import Column, types
 from sqlalchemy.dialects.postgresql import HSTORE
 from sqlalchemy_utc import UtcDateTime
 
 from pydantic_sqlalchemy.field import infer_python_type
-
-
-def test_direct_python_type_is_preferred_to_impl() -> None:
-    # Arrange
-    column = Column("col", Interval)
-
-    # Act
-    python_type = infer_python_type(column)
-
-    # Assert
-    assert python_type is dt.timedelta
 
 
 def test_fallback_to_python_type_from_impl() -> None:
@@ -38,7 +28,7 @@ def test_enum() -> None:
         FALSE = "F"
         TRUE = "T"
 
-    boolean = Column("bool", Enum(Bool))
+    boolean = Column("bool", types.Enum(Bool))
 
     # Act
     python_type = infer_python_type(boolean)
@@ -47,9 +37,68 @@ def test_enum() -> None:
     assert python_type is Bool
 
 
+@pytest.mark.parametrize(
+    "sa_type,expected_type",
+    [
+        (types.BigInteger, int),
+        (types.Boolean, bool),
+        (types.Date, dt.date),
+        (types.DateTime, dt.datetime),
+        # (types.Enum, enum.Enum),
+        (types.Float, float),
+        (types.Integer, int),
+        (types.Interval, dt.timedelta),
+        (types.LargeBinary, bytes),
+        # (types.MatchType, bool),
+        (types.Numeric, Decimal),
+        (types.PickleType, bytes),
+        # (types.SchemaType, None),
+        (types.SmallInteger, int),
+        (types.String, str),
+        (types.Text, str),
+        (types.Time, dt.time),
+        (types.Unicode, str),
+        (types.UnicodeText, str),
+        # (types.ARRAY, list),
+        (types.BIGINT, int),
+        (types.BINARY, bytes),
+        (types.BLOB, bytes),
+        (types.BOOLEAN, bool),
+        (types.CHAR, str),
+        (types.CLOB, str),
+        (types.DATE, dt.date),
+        (types.DATETIME, dt.datetime),
+        (types.DECIMAL, Decimal),
+        (types.FLOAT, float),
+        (types.INT, int),
+        (types.INTEGER, int),
+        (types.JSON, dict),
+        (types.NCHAR, str),
+        (types.NUMERIC, Decimal),
+        (types.NVARCHAR, str),
+        (types.REAL, float),
+        (types.SMALLINT, int),
+        (types.TEXT, str),
+        (types.TIME, dt.time),
+        (types.TIMESTAMP, dt.datetime),
+        (types.VARBINARY, bytes),
+        (types.VARCHAR, str),
+    ]
+)
+def test_common_types(sa_type: types.TypeEngine, expected_type: type) -> None:  # type: ignore[type-arg]
+    # Arrange
+    column = Column("column", sa_type)
+
+    # Act
+    inferred_type = infer_python_type(column)
+
+    # Assert
+    assert inferred_type is expected_type
+
+
 def test_array() -> None:
     # Arrange
-    array = Column("array", ARRAY(item_type=Integer))
+    array = Column("array", types.ARRAY(item_type=types.Integer))
 
     # Act
     python_type = infer_python_type(array)
