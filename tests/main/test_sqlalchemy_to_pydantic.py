68d118d8cb8da102f1708b631f3317bb4d081d47
Gabriel Galli <ggabriel96@hotmail.com>
2021-05-22 12:15:26 -0300

    Testing model name

--- tests/main/test_sqlalchemy_to_pydantic.py
+++ tests/main/test_sqlalchemy_to_pydantic.py
@@ -5,6 +5,40 @@ from sqlalchemy.orm import declarative_base
 from alchemista import sqlalchemy_to_pydantic
 
 
+def test_model_names_come_from_dunder_name() -> None:
+    # Arrange
+    Base = declarative_base()
+
+    class ModelA(Base):
+        __tablename__ = "a"
+        id = Column(Integer, primary_key=True)
+
+    class ModelB(Base):
+        __tablename__ = "b"
+        id = Column(Integer, primary_key=True)
+
+    # Act
+    ModelAPydantic = sqlalchemy_to_pydantic(ModelA)
+    ModelBPydantic = sqlalchemy_to_pydantic(ModelB)
+
+    # Assert
+    assert ModelAPydantic.__name__ == "ModelA"
+    assert ModelAPydantic.schema() == {
+        "title": "ModelA",
+        "type": "object",
+        "properties": {"id": {"title": "Id", "type": "integer"}},
+        "required": ["id"],
+    }
+
+    assert ModelBPydantic.__name__ == "ModelB"
+    assert ModelBPydantic.schema() == {
+        "title": "ModelB",
+        "type": "object",
+        "properties": {"id": {"title": "Id", "type": "integer"}},
+        "required": ["id"],
+    }
+
+
 def test_exclude_removes_fields_from_generated_model() -> None:
     # Arrange
     Base = declarative_base()
