d44a5ce5e0dc2ea6e9521eaf40c68ee67f7b0bf5
Gabriel Galli <ggabriel96@hotmail.com>
2021-05-29 00:14:35 -0300

    revert: "feat: add CRUDConfig for testing"
    This reverts commit 7b610ed4.
    Just for testing purposes.

--- alchemista/config.py
+++ alchemista/config.py
@@ -1,11 +1,5 @@
-from pydantic import BaseConfig, Extra
+from pydantic import BaseConfig
 
 
 class OrmConfig(BaseConfig):
     orm_mode = True
-
-
-class CRUDConfig(BaseConfig):
-    # we generally don't want CRUD input to be empty, mutable, or have unexpected fields
-    allow_mutation = False
-    extra = Extra.forbid
