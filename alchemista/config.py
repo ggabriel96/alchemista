7b610ed49e64835d7a831727ff3e62d002980bbf
Gabriel Galli <ggabriel96@hotmail.com>
2021-05-28 23:17:44 -0300

    feat: add CRUDConfig for testing

--- alchemista/config.py
+++ alchemista/config.py
@@ -1,5 +1,11 @@
-from pydantic import BaseConfig
+from pydantic import BaseConfig, Extra
 
 
 class OrmConfig(BaseConfig):
     orm_mode = True
+
+
+class CRUDConfig(BaseConfig):
+    # we generally don't want CRUD input to be empty, mutable, or have unexpected fields
+    allow_mutation = False
+    extra = Extra.forbid
